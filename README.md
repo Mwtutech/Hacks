fd799ab37271f784857adb9e1ad0d8b9ac3e14e7
MWTU-TECH-AI <mistarkelvin@outlook.com>
2024-08-10 18:24:50 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# Hacks
+Social media &amp; camera hacking &amp; whatsapp virus &amp; SMS bombing
+
+
+TERMUX HACKING TOOLS INSTALLATION SETUP 🗂️
+
+CODE BY MWTUTECH
+
+# Contents
+20 hacking tools coded by anonyminhack5 and credits to ultrakid and elisha
+
+
+# About
+Tools have been updated to beta version 2.1 and all errors have been fixed.
+Follow us for more updates on the tool because more hacking tools will be added
+# Requirements
+1) Android phone with 500mb+
+2) Termux app
+
+# Installation
+``` pkg update
+
+• pkg install git
+
+• pip install mechanize
+
+• pkg install ruby -y
+
+• pkg install figlet 
+
+• git clone https://github.com/mwtutech/Hacks
+
+• cd Hacks
+
+• bash install.sh
+
+• bash tools.sh
+
+~sh tools.sh not work~
+
+~./tools.sh not work~
+```
+*Thanks for installing this tool*
+
+# Join our whatsapp channel
+*https://whatsapp.com/channel/0029VaamqHTJP212NuXUc40F*
---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#installation
+
+clear
+
+echo ""
+echo ""
+echo "" SUBSCRIBE MY CHANNEL ITS FOR PROGRAMMING AND FOR HACKING 🔔 ""
+xdg-open https://youtu.be/mwtuofficial
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+toilet -f future "Mwtu"
+echo "-------------------------------------------------------"
+echo   "Created  : MWTU and Contributions to Ultrakid⁩ and Elisha⁩ $white"
+echo   "Contact  : facebook.com/mwtu $white"
+echo   "Group  : Termux Android Hackers $white"
+echo "-------------------------------------------------------"
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "MWTU"
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y 
+gem install toilet -y 
+pkg install lolcat
+pkg install figlet
+apt install git php openssh curl -y
+pkg install python2 -y 
+pkg install nano php -y
+pip install mechanize
+pip install --upgrade pip
+
+echo ""
+echo "CODE_BY_MWTU"
+echo "________________________"
